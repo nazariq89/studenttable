d07b37781dbd1ce65016b32de9f3cb53680ef9c7
Nazariq <ahmadnazariq@gmail.com>
2022-08-29 14:58:34 +0800

    Second Testing

--- src/main/java/com/zariqmattproject/studenttable/model/StudentAPI.java
+++ src/main/java/com/zariqmattproject/studenttable/model/StudentAPI.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/student")
+@RequestMapping(value = "/student")  //student
 
 public class StudentAPI {
 
